8fea79bbb959be1cbed941f190b55baea6893221
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 18:12:29 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -modcacherw -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/aqua
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/aqua
 go-licenses save ./cmd/aqua --save_path=license-files
